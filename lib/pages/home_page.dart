ecf23f2bf6203b3555fed4d9eda74d04fde7262c
Fernando <cardosofm@gmail.com>
2021-04-14 17:18:46 -0300

    montagem da tela de listagem de contatos

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:contact_list_sqlite/helpers/contact_helper.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -7,28 +8,53 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // instancia da classe helper
   ContactHelper helper = ContactHelper();
+  // criação de uma lista vazia que receberá o conteudo do banco de dados
+  List<Contact> contacts = [];
 
   @override
   void initState() {
     super.initState();
-    /*Contact c = Contact();
+    // colocando todos os contatos na lista contacts
+    helper.getAllContacts().then((list) {
+      setState(() {
+        contacts = list;
+      });
+    });
+  }
+
+  /*
+  @override
+  void initState() {
+    super.initState();
+    */ /*Contact c = Contact();
     c.nome = "Fernando";
     c.email = "fernando@bruner.com.br";
     c.telefone = "123456";
     c.img = "";
-    helper.saveContact(c);*/
+    helper.saveContact(c);*/ /*
 
     helper.getAllContacts().then((list) {
       print(list);
     });
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Contact List with SQLite'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      body: Container(
+        child: ListView.builder(
+            itemCount: contacts.length,
+            padding: EdgeInsets.all(10),
+            itemBuilder: (context, index) {}),
       ),
     );
   }
